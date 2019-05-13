59f05659ddb2ca4d8a67de5eb21776e9c163e9d0
David <dhahn123@gmail.com>
2019-05-12 18:19:21 -0700

    simplify App.tsx page, remove CSS, include Router

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -1,20 +1,14 @@
-import { observer } from 'mobx-react-lite';
-import React, { useContext } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { CounterStoreContext } from './stores/CounterStore';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Router } from './Router';
 
-export const App = observer(() => {
-  const counterStore = useContext(CounterStoreContext);
-
+export const App = () => {
   return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{counterStore.count}</Text>
-        <Button title="increment" onPress={() => counterStore.count++} />
-      </View>
+    <View style={styles.container}>
+    <Router />
+    </View>
   )
-});
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -22,15 +16,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
