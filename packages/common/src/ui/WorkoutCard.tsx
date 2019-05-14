5c5f9a2d507b8a0408e52a9210d49521242e7065
David <dhahn123@gmail.com>
2019-05-13 20:49:24 -0700

    Create Card Container

--- packages/common/src/ui/WorkoutCard.tsx
+++ packages/common/src/ui/WorkoutCard.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import * as React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Card } from "./Card";
 
 interface Props {
     exercise: string;
@@ -10,15 +11,7 @@ interface Props {
 }
 
 const styles = StyleSheet.create({
-  card: {
-    borderRadius: 3,
-    backgroundColor: "#fff",
-    shadowColor: "#000",
-    shadowOffSet: { width: 2, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    flexDirection: "column",
-    padding: 10,
+  cardContainer: {
     marginBottom: 10
   },
   topRow: {
@@ -58,7 +51,8 @@ const styles = StyleSheet.create({
 
 export const WorkoutCard: React.FC<Props> = observer(({exercise, repsAndWeight, sets, onSetPress}) => {
   return (
-    <View style={styles.card}>
+    <View style={styles.cardContainer}>
+    <Card>
         <View style={styles.topRow}>
             <Text style={styles.topRowText}>{exercise}</Text>
             <Text style={styles.topRowText}>{repsAndWeight}</Text>
@@ -87,6 +81,7 @@ export const WorkoutCard: React.FC<Props> = observer(({exercise, repsAndWeight, 
                 );
             })}
     </View>
+    </Card>
     </View>
   )
 });
